660c2a2aec6257e1fb0256a6cbd41cc9b8ef6cfe
Hello <chi876543@gmail.com>
2026-03-16 23:21:04 +0700

    validate create

--- backend/app/Http/Requests/StoreRechargeBillRequest.php
+++ backend/app/Http/Requests/StoreRechargeBillRequest.php
@@ -11,7 +11,7 @@ class StoreRechargeBillRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,10 @@ class StoreRechargeBillRequest extends FormRequest
         return [
             'money' => 'required|numeric|min:0.01',
             'total_money' => 'required|numeric|min:0.01',
+            'status' => 'required|in:completed,failed',
             'vat' => 'required|numeric|min:0',
+            'recharge_rule_id' => 'required|exists:recharge_rules,id',
+            'account_id' => 'required|exists:accounts,id',
         ];
     }
 }
--- backend/app/Http/Requests/StoreRechargeRuleRequest.php
+++ backend/app/Http/Requests/StoreRechargeRuleRequest.php
@@ -11,7 +11,7 @@ class StoreRechargeRuleRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,7 @@ class StoreRechargeRuleRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'point' => 'required|integer|min:1',
+            'points' => 'required|integer|min:1',
             'money' => 'required|numeric|min:0.01',
         ];
     }

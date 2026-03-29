a6e68e8e82e1357ee36b25dd0e0ab6f4d8ec452c
Phạm Nguyễn Phú Thành <thanhphamnguyenphu2k5@gmail.com>
2026-03-29 12:48:49 +0700

    Update index.php

--- frontend/pages/client/index.php
+++ frontend/pages/client/index.php
@@ -34,97 +34,6 @@
   <body>
     <?php include(__DIR__ . "/components/header.php"); ?>
 
-    <div class="filter-background">
-      <div class="filter">
-        <div class="filter-return-block">
-          <div class="filter-return">
-            <img src='<?php echo BASE_URL . "/assets/img/return-ico.png"?>' alt="return-ico.png" class="filter-return-ico">
-
-            Quay lại
-          </div>
-        </div>
-
-        <!-- <div class="filter-line"></div> -->
-
-        <div class="filter-area">Khu vực</div>
-
-        <div class="filter-province">
-          <input type="checkbox" name="filter-province-cb" id="filter-province-cb" class="filter-province-cb">
-
-          <label for="filter-province-cb" class="filter-province-lb">
-            Chọn tỉnh thành
-
-            <img src='<?php echo BASE_URL . "/assets/img/arrow_bold.png"?>' alt="arrow.png" class="filter-arrow">
-          </label>
-
-          <ul class="filter-province-list">
-            <!-- an example item -->
-             <?php for ($i = 1; $i <= 10; $i++) { ?>
-            <li class="filter-province-item">Thành phố Hồ Chí Minh</li>
-            <?php } ?>
-          </ul>
-        </div>
-
-        <div class="filter-district">
-          <input type="checkbox" name="filter-district-cb" id="filter-district-cb" class="filter-district-cb">
-
-          <label for="filter-district-cb" class="filter-district-lb">
-            Chọn phường xã
-
-            <img src='<?php echo BASE_URL . "/assets/img/arrow_bold.png"?>' alt="arrow.png" class="filter-arrow">
-          </label>
-
-          <ul class="filter-district-list">
-            <!-- an example item -->
-            <li class="filter-district-item">Phường Sài Gòn</li>
-          </ul>
-        </div>
-
-        <!-- <div class="filter-line"></div> -->
-
-        <div class="filter-room">Loại phòng</div>
-
-        <div class="filter-rooms">
-          <input type="checkbox" name="filter-room-cb" id="filter-room-cb" class="filter-room-cb">
-
-          <label for="filter-room-cb" class="filter-room-lb">
-            Chọn loại phòng
-
-            <img src='<?php echo BASE_URL . "/assets/img/arrow_bold.png"?>' alt="arrow.png" class="filter-arrow">
-          </label>
-
-          <ul class="filter-room-list">
-            <!-- an example item -->
-            <li class="filter-room-item">Phòng đơn</li>
-          </ul>
-        </div>
-
-        <!-- <div class="filter-line"></div> -->
-
-        <div class="filter-price">Giá cả</div>
-
-        <input type="number" name="filter-min-price" id="filter-min-price" 
-        placeholder="Giá nhỏ nhất" 
-        min="0" class="filter-min-price">
-
-        <input type="number" name="filter-max-price" id="filter-max-price" 
-        placeholder="Giá lớn nhất" 
-        min="0" class="filter-max-price">
-
-        <!-- <div class="filter-line"></div> -->
-
-        <div class="filter-square">Diện tích</div>
-
-        <input type="number" name="filter-square-number" id="filter-square-number" 
-        placeholder="Nhập diện tích" 
-        min="0" class="filter-square-number">
-
-        <!-- <div class="filter-line"></div> -->
-
-        <button type="button" class="filter-apply">Áp dụng</button>
-      </div>
-    </div>
-
     <div class="hero">
       <div class="hero-content">
         <div class="content-title">
@@ -139,40 +48,6 @@
             Tất cả những gì bạn cần để tìm chỗ ở đều có ở<br />
             đây, giúp bạn dễ dàng hơn trong việc tìm kiếm
           </h3>
-        </div>
-
-        <div class="content-search">
-          <div class="search-tool">
-            <img
-              src='<?php echo BASE_URL . "/assets/img/Find.png" ?>'
-              alt="find.png"
-              class="search-ico"
-            />
-
-            <input
-              type="text"
-              name=""
-              id=""
-              placeholder="Tìm kiếm"
-              class="search-bar"
-            />
-          </div>
-
-          <div class="search-seperate"></div>
-
-          <div class="other-tools">
-            <div class="filter-tool">
-              <img
-                src='<?php echo BASE_URL . "/assets/img/Vector.png" ?>'
-                alt="filter.png"
-                class="filter-ico"
-              />
-
-              <div class="filter-text">Bộ lọc</div>
-            </div>
-
-            <button class="search-submit">Tìm phòng</button>
-          </div>
         </div>
       </div>
 
@@ -226,47 +101,6 @@
             </div>
           </a>
         </div>
-
-        <?php for ($i = 1; $i <= 4; $i++) { ?>
-        <div class="post">
-          <div class="post-favour">
-            <img
-              src='<?php echo BASE_URL . "/assets/img/unfavour.png" ?>'
-              alt="favour.png"
-              class="post-favour-ico"
-            />
-          </div>
-
-          <a href="./detail-post.php" class="post-body">
-            <img
-              src='<?php echo BASE_URL . "/assets/img/post.png" ?>'
-              alt="post.png"
-              class="post-img"
-            />
-
-            <div class="post-title">
-              Phòng giá 5 tỷ/ tháng, đường Trương Phước Phan, phường Bình Trị
-              Đông, quận Bình Tân
-            </div>
-
-            <div class="post-address">
-              <img
-                src='<?php echo BASE_URL . "/assets/img/address.png" ?>'
-                alt="address.png"
-                class="address-ico"
-              />
-
-              <div class="address-info">Phường Bình Trị Đông, Tp Hồ Chí Minh</div>
-            </div>
-
-            <div class="post-info">
-              <h3 class="post-price">5 tỷ/tháng</h3>
-
-              <div class="post-square">57.5 m2</div>
-            </div>
-          </a>
-        </div>
-        <?php } ?>
       </div>
 
       <a href='<?php echo BASE_URL . "/pages/client/posts.php"?>' class="more">Xem thêm</a>
@@ -274,7 +108,7 @@
       <div class="suggest">
         <div class="suggest-text">Muốn nhận bài đăng phù hợp?</div>
 
-        <a href="./index.php" class="suggest-link"> Thêm đề xuất của bạn</a>
+        <a href='<?php echo BASE_URL . "/pages/client/suggest.php"?>' class="suggest-link"> Thêm đề xuất của bạn</a>
       </div>
     </div>
 
@@ -324,46 +158,6 @@
             </div>
           </a>
         </div>
-
-        <?php for ($i = 1; $i <= 4; $i++) { ?>
-        <div class="post">
-          <div class="post-favour">
-            <img
-              src='<?php echo BASE_URL . "/assets/img/unfavour.png" ?>'
-              alt="favour.png"
-              class="post-favour-ico"
-            />
-          </div>
-
-          <a href="./detail-post.php" class="post-body">
-            <img
-              src='<?php echo BASE_URL . "/assets/img/post.png" ?>'
-              alt="post.png"
-              class="post-img"
-            />
-
-            <div class="post-title">
-              Phòng giá 5 tỷ/ tháng, đường Trương Phước Phan, phường Bình Trị
-              Đông, quận Bình Tân
-            </div>
-
-            <div class="post-address">
-              <img
-                src='<?php echo BASE_URL . "/assets/img/address.png" ?>'
-                alt="address.png"
-                class="address-ico"
-              />
-
-              <div class="address-info">Phường Bình Trị Đông, Tp Hồ Chí Minh</div>
-            </div>
-            <div class="post-info">
-              <h3 class="post-price">5 tỷ/tháng</h3>
-
-              <div class="post-square">57.5 m2</div>
-            </div>
-          </a>
-        </div>
-        <?php } ?>
       </div>
 
       <a href='<?php echo BASE_URL . "/pages/client/suggest-posts.php"?>' class="more">Xem thêm</a>
@@ -372,25 +166,7 @@
     <?php include(__DIR__ . "/components/footer.php") ?>
   </body>
   <script>
-    // filter script
-    const filter_button = document.querySelector(".filter-tool");
-    const filter = document.querySelector(".filter-background");
-    const filter_return = document.querySelector(".filter-return");
-
-    filter_button.addEventListener("click", (e) => {
-      if(filter.style.display == "flex") {
-        filter.style.display = "none"
-      } else {
-        filter.style.display = "flex"
-      }
-    });
-
-    filter_return.addEventListener("click", (e) => {
-      if(filter.style.display == "flex") {
-        filter.style.display = "none"
-      } else {
-        filter.style.display = "flex"
-      }
-    });
+    // get all posts to posts section (do not require token)
+    const response = await fetch("http://127.0.0.1:8000/api/posts", {})
   </script>
 </html>

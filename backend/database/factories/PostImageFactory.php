950e5212fa0cbd6e02efed15e7363e2e176be247
Hello <chi876543@gmail.com>
2026-04-02 08:10:51 +0700

    fix current @extends

--- backend/database/factories/PostImageFactory.php
+++ backend/database/factories/PostImageFactory.php
@@ -7,7 +7,7 @@ use App\Models\Posts\PostImage;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\PostImage>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Posts\PostImage>
  */
 class PostImageFactory extends Factory
 {
--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Account_User\Account>
  */
 class UserFactory extends Factory
 {

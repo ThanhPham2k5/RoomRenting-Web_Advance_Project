cfcf70addc22f9e620f6248c1eb4ec4fbd2f77b3
Phạm Nguyễn Phú Thành <thanhphamnguyenphu2k5@gmail.com>
2026-04-03 14:26:14 +0700

    temp

--- frontend/pages/client/history.php
+++ frontend/pages/client/history.php
@@ -80,34 +80,6 @@
                 <h3 class="history-board-title">Lịch sử nạp</h3>
 
                 <div class="history-notify-list">
-                    <!-- an example item -->
-                    <a href="" class="notify-item">
-                        <img src='<?php echo BASE_URL . "/assets/img/left-img.png"?>' alt="item-img.png" class="notify-item-img">
-
-                        <div class="notify-content">
-                        <div class="notify-info">Thêm điểm vào tài khoản thành công</div>
-
-                        <!-- using for transaction -->
-                        <div class="notify-value value-add">+999 Điểm ( Nạp thêm điểm thành công )</div>
-
-                        <div class="notify-time">01/01/2026</div>
-                        </div>
-                    </a>
-
-                    <?php for ($i = 1; $i <= 10; $i++) { ?>
-                    <a href="" class="notify-item">
-                        <img src='<?php echo BASE_URL . "/assets/img/left-img.png"?>' alt="item-img.png" class="notify-item-img">
-
-                        <div class="notify-content">
-                        <div class="notify-info">Thêm điểm vào tài khoản thành công</div>
-
-                        <!-- using for transaction -->
-                        <div class="notify-value value-subtract">-100 Điểm ( Phí sử dụng dịch vụ )</div>
-
-                        <div class="notify-time">01/01/2026</div>
-                        </div>
-                    </a>
-                    <?php } ?>
                 </div>
 
                 <div class="pagination">
@@ -136,8 +108,195 @@
     <?php include(__DIR__ . "/components/footer.php") ?>
   </body>
   <script>
-    async function getBill(account_id, token, billType) {
-        
+    // filter & sort & page value
+    var page = 1
+    var lastPage = 1
+
+    // pagination button
+    document.querySelector(".page-extra-prev").addEventListener("click", async (e) => {
+      page = 1
+      await updateHistoryPage()
+    })
+
+    document.querySelector(".page-prev").addEventListener("click", async (e) => {
+      page = Math.max(1, page - 1)
+      await updateHistoryPage()
+    })
+
+    document.querySelector(".page-extra-next").addEventListener("click", async (e) => {
+      page = lastPage
+      await updateHistoryPage()
+    })
+
+    document.querySelector(".page-next").addEventListener("click", async (e) => {
+      page = Math.min(lastPage, page + 1)
+      await updateHistoryPage()
+    })
+    
+    document.querySelector(".page-prev-number").addEventListener("click", async (e) => {
+      page = parseInt(document.querySelector(".page-prev-number").textContent.trim())
+      await updateHistoryPage()
+    })
+    
+    document.querySelector(".page-current-number").addEventListener("click", async (e) => {
+      page = parseInt(document.querySelector(".page-current-number").textContent.trim())
+      await updateHistoryPage()
+    })
+    
+    document.querySelector(".page-next-number").addEventListener("click", async (e) => {
+      page = parseInt(document.querySelector(".page-next-number").textContent.trim())
+      await updateHistoryPage()
+    })
+
+    document.querySelector(".page-number").addEventListener("change", async (e) => {
+      const pageNumber = document.querySelector(".page-number").value.trim()
+      if(!pageNumber)
+        page = 1
+      else 
+        page =  Math.min(lastPage, Math.max(1, parseInt(pageNumber)))
+
+      document.querySelector(".page-number").value = page
+      await updateHistoryPage()
+    })
+
+    function updatePageNumber() {
+      const prevPage = document.querySelector(".page-prev-number")
+      const currPage = document.querySelector(".page-current-number")
+      const nextPage = document.querySelector(".page-next-number")
+
+      // only 1 page
+      if (page === 1 && lastPage === 1) {
+          prevPage.textContent = 1
+          prevPage.classList.add("page-selected")
+          currPage.textContent = 2
+          currPage.classList.remove("page-selected")
+          currPage.classList.add("page-none")
+          nextPage.textContent = 3
+          nextPage.classList.remove("page-selected")
+          nextPage.classList.add("page-none")
+
+      // 2 page
+      } else if (lastPage === 2) {
+          nextPage.textContent = 3
+          nextPage.classList.remove("page-selected")
+          nextPage.classList.add("page-none")
+
+          if (page === 1) {
+              prevPage.textContent = 1
+              prevPage.classList.add("page-selected")
+              currPage.textContent = 2
+              currPage.classList.remove("page-selected")
+              currPage.classList.remove("page-none")
+          } else {
+              prevPage.textContent = 1
+              prevPage.classList.remove("page-selected")
+              currPage.textContent = 2
+              currPage.classList.add("page-selected")
+              currPage.classList.remove("page-none")
+          }
+
+      // 3+ page
+      } else {
+          currPage.classList.remove("page-none")
+          nextPage.classList.remove("page-none")
+
+          if (page === 1) {
+              prevPage.textContent = 1
+              prevPage.classList.add("page-selected")
+              currPage.textContent = 2
+              currPage.classList.remove("page-selected")
+              nextPage.textContent = 3
+              nextPage.classList.remove("page-selected")
+          } else if (page === lastPage) {
+              nextPage.textContent = lastPage
+              nextPage.classList.add("page-selected")
+              currPage.textContent = lastPage - 1
+              currPage.classList.remove("page-selected")
+              prevPage.textContent = lastPage - 2
+              prevPage.classList.remove("page-selected")
+          } else if (page > 1 && page < lastPage) {
+              currPage.textContent = page
+              currPage.classList.add("page-selected")
+              prevPage.textContent = page - 1
+              prevPage.classList.remove("page-selected")
+              nextPage.textContent = page + 1
+              nextPage.classList.remove("page-selected")
+          }
+      }
     }
+
+    async function getBill(account_id, token, billType, page) {
+        try {
+            const response = await fetch("http://127.0.0.1:8000/api/" + billType + "?per_page=10&include=account&filter[account.id]=" + account_id + "&filter[status]=completed&page=" + page, {
+                method: "GET",
+                headers: {
+                    "Accept": "application/json",
+                    "Authorization": "Bearer " + token
+                }
+            })
+
+            const data = await response.json()
+            if(response.ok) {
+                if(data.data) {
+                    lastPage = data.meta.last_page
+                    return data.data
+                }
+            } else {
+                console.error(data)
+            }
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
+    async function updateHistoryPage() {
+        const account_id = localStorage.getItem("account_id")
+        const token = localStorage.getItem("token")
+
+        if(account_id != null && token != null) {
+            var billType = document.querySelector(".history-recharge").classList.contains("history-selected") ? "rechargeBills" : "payBills"
+            var valueItem = document.querySelector(".history-recharge").classList.contains("history-selected") ? "value-add" : "value-subtract"
+            var billTitle = document.querySelector(".history-recharge").classList.contains("history-selected") ? "Thêm điểm" : "Trừ điểm"
+
+            const histories = await getBill(account_id, token, billType, page)
+
+            if(histories != null && histories.length > 0) {
+                let html = ""
+
+                histories.forEach(history => {
+                    var point = Number(history.points).toLocaleString("vi-VN")
+                    html = `
+                        <div class="notify-item">
+                            <img src='<?php echo BASE_URL . "/assets/img/transaction-img.png"?>' alt="item-img.png" class="notify-item-img">
+
+                            <div class="notify-content">
+                            <div class="notify-info">${billTitle} vào tài khoản thành công</div>
+
+                            <!-- using for transaction -->
+                            <div class="notify-value ${valueItem}">+${point} Điểm ( Nạp thêm điểm thành công )</div>
+
+                            <div class="notify-time">01/01/2026</div>
+                            </div>
+                        </div>
+                    `
+                });
+
+                document.querySelector(".history-notify-list").innerHTML = html
+            } else {
+                document.querySelector(".history-notify-list").textContent = "Chưa có lịch sử giao dịch."
+            }
+        } else {
+            alert("Bạn chưa đăng nhập. Đang chuyển hướng sang trang đăng nhập.")
+            window.location.href = "login.php"
+        }
+    }
+
+    // run once time every reload or load page
+    document.addEventListener("DOMContentLoaded", async (e) => {
+      var account_id = localStorage.getItem("account_id")
+      var token = localStorage.getItem("token")
+
+      await updateHistoryPage()
+    })
   </script>
 </html>
